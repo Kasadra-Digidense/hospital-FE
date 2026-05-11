8e1fb765ecf6f0dd9031b9fad68b3cdd2a60815e
shabukumaresan <shabuk2002@gmail.com>
2026-05-11 14:15:29 +0530

    new

---
+++ Ayurvedhic/src/api/invoiceAPI.js
@@ -0,0 +1,14 @@
+import axiosInstance from "./axiosInstance";
+
+export const fetchInvoicePatientsApi = async () => {
+  try {
+    const response = await axiosInstance.get("/patients/");
+    return response.data;
+  } catch (error) {
+    if (!error.response) {
+      throw new Error("Unable to reach the server.");
+    }
+
+    throw new Error(error.response?.data?.detail || "Failed to fetch patients");
+  }
+};
