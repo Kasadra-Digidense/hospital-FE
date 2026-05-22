81875adf9de82e5c32f6a28d34dc1820b681ba40
jinisha-a-s <jinisha.agustinraj@digidense.in>
2026-05-22 10:57:33 +0530

    treatments changed

--- Ayurvedhic/src/layouts/Sidebar.jsx
+++ Ayurvedhic/src/layouts/Sidebar.jsx
@@ -53,27 +53,6 @@ const Sidebar = () => {
             <p>Dashboard</p>
           </NavLink>
 
-          <NavLink
-            to="/doctor-register"
-            className={({ isActive }) =>
-              `sidebar-item ${isActive ? "sidebar-active" : ""}`
-            }
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-            <p>Register Doctor</p>
-          </NavLink>
 
           <NavLink
             to="/patient-register"
@@ -125,6 +104,30 @@ const Sidebar = () => {
             </svg>
             <p>Create Invoice</p>
           </NavLink>
+
+          <NavLink
+            to="/doctor-register"
+            className={({ isActive }) =>
+              `sidebar-item ${isActive ? "sidebar-active" : ""}`
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            </svg>
+            <p>Register Doctor</p>
+          </NavLink>
+
+          
 
           <NavLink
             to="/treatments"
